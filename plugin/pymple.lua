9bf62731823a22c1970e74a43f770853e2ea3d9a
Alexandre Pasmantier <alex.pasmant@gmail.com>
2024-07-21 20:33:08 +0200

    fix: remove nargs entirely

--- plugin/pymple.lua
+++ plugin/pymple.lua
@@ -1,13 +1,11 @@
 vim.api.nvim_create_user_command("UpdatePythonImports", function(opts)
   require("pymple.api").update_imports(opts.fargs[1], opts.fargs[2])
 end, {
-  nargs = "2",
   desc = "Update all imports in workspace after renaming `source` to `destination`",
 })
 
 vim.api.nvim_create_user_command("ResolvePythonImportUnderCursor", function(_)
   require("pymple.api").resolve_import()
 end, {
-  nargs = "0",
   desc = "Resolves import for symbol under cursor",
 })
