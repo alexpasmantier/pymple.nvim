d5a70002ba68f80d2b0f8454195553c0ab8cf247
alexpasmantier <alex.pasmant@gmail.com>
2024-08-10 13:27:07 +0200

    fix

--- lua/pymple/project.lua
+++ lua/pymple/project.lua
@@ -11,7 +11,7 @@ function ProjectState:setup()
   self.venv_location = utils.get_virtual_environment(cwd)
   log.debug("Virtual environment: " .. (self.venv_location or "None"))
   local p_root = utils.find_project_root(cwd)
-  log.debug("Project root: " .. p_root)
+  log.debug("Project root: " .. (p_root or "None"))
   if p_root == nil then
     log.warn(
       "Unable to find project root, defaulting to current working directory."
