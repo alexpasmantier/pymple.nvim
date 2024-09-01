164a65c1a979b9e4c37e16f3fbb903e4f0a6ecd0
alexpasmantier <alex.pasmant@gmail.com>
2024-09-01 02:21:01 +0200

    fix(resolution): use `vim.system` instead of plenary Job

--- lua/pymple/resolve_imports/jobs.lua
+++ lua/pymple/resolve_imports/jobs.lua
@@ -1,4 +1,3 @@
-local Job = require("plenary.job")
 local log = require("pymple.log")
 local utils = require("pymple.utils")
 
@@ -9,20 +8,20 @@ function M.find_import_candidates(gg_args, sys_paths)
   for _, sys_path in ipairs(sys_paths) do
     local gg_command = "gg " .. gg_args .. sys_path
     log.debug("GG command: " .. gg_command)
-    local job = Job:new({
-      command = utils.SHELL,
-      args = { "-c", gg_command },
-      on_exit = function(job, _)
-        local results = job:result()
-        if #results ~= 0 then
-          for _, result in ipairs(results) do
-            local path = utils.make_relative_to(result, sys_path)
-            table.insert(candidates, path)
-          end
-        end
-      end,
-    })
-    job:sync()
+
+    local job = vim.system({ utils.SHELL, "-c", gg_command }):wait()
+    local result_lines = vim.split(job.stdout, "\n")
+    for i, line in ipairs(result_lines) do
+      if line == "" then
+        table.remove(result_lines, i)
+      end
+    end
+    if #result_lines ~= 0 then
+      for _, line in ipairs(result_lines) do
+        local path = utils.make_relative_to(line, sys_path)
+        table.insert(candidates, path)
+      end
+    end
   end
   return candidates
 end
